eb4234dc917fe9c575ca603c187053af1797c425
LeoBessa403 <leonardomcbessa@gmail.com>
2021-03-01 12:35:28 -0300

    IGNORE

--- BancoDados/Atualizacao.sql
+++ BancoDados/Atualizacao.sql
@@ -10,3 +10,25 @@ UPDATE ccaug100.TB_ACESSO SET dt_fim_acesso = "2021-03-01 12:30:14" where co_ace
 
 UPDATE ccaug100.TB_ACESSO SET dt_fim_acesso = "2021-03-01 12:30:15" where co_acesso = 21;
 
+UPDATE ccaug100.TB_ACESSO SET tp_situacao = "F", dt_fim_acesso = "2021-03-01 12:30:17" where co_acesso = 21;
+
+INSERT INTO ccaug100.TB_TRAFEGO (ds_navegador, ds_sistema_operacional, ds_dispositivo, nu_ip, ds_pais, ds_code_pais, ds_estado, ds_uf, ds_cidade, ds_agente) VALUES ("Firefox", "Windows 8.1", "Desktop", "127.0.0.1", "Desconhecido", "Desconhecida", "Desconhecida", "Desconhecida", "Desconhecida", "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0");
+
+INSERT INTO ccaug100.TB_ACESSO (ds_session_id, co_usuario, tp_situacao, co_trafego, dt_fim_acesso, dt_inicio_acesso) VALUES ("m6h53bt5v2hd4631ambgn9sk3n", "1", "A", "22", "2021-03-01 13:04:53", "2021-03-01 12:34:53");
+
+UPDATE ccaug100.TB_ACESSO SET dt_fim_acesso = "2021-03-01 13:04:54" where co_acesso = 22;
+
+UPDATE ccaug100.TB_ACESSO SET dt_fim_acesso = "2021-03-01 13:04:55" where co_acesso = 22;
+
+UPDATE ccaug100.TB_ACESSO SET dt_fim_acesso = "2021-03-01 13:04:58" where co_acesso = 22;
+
+UPDATE ccaug100.TB_ACESSO SET dt_fim_acesso = "2021-03-01 13:04:59" where co_acesso = 22;
+
+UPDATE ccaug100.TB_ACESSO SET dt_fim_acesso = "2021-03-01 13:05:04" where co_acesso = 22;
+
+UPDATE ccaug100.TB_ACESSO SET dt_fim_acesso = "2021-03-01 13:05:05" where co_acesso = 22;
+
+UPDATE ccaug100.TB_ACESSO SET dt_fim_acesso = "2021-03-01 13:05:07" where co_acesso = 22;
+
+UPDATE ccaug100.TB_ACESSO SET dt_fim_acesso = "2021-03-01 13:05:07" where co_acesso = 22;
+
--- library/Helpers/includes/validacoes.js
+++ library/Helpers/includes/validacoes.js
@@ -213,13 +213,13 @@ $(function () {
 
     $(".cartao_credito").mask("9999 9999 9999 9999");
 
-    $(".cvv").attr('maxlength', 3).attr('placeholder', '000').keyup(function () {
+    $(".cvv").mask("999").keyup(function () {
         var valor = $(this).val().replace(/[^0-9]+/g, '');
         valor = valor.val().replace(/[^.-]+/g, '');
         $(this).val(valor);
     });
 
-    $(".validade_cartao").attr('maxlength', 7).attr('placeholder', '00/0000').keyup(function () {
+    $(".validade_cartao").mask("99/9999").keyup(function () {
         var valor = $(this).val().replace(/[^0-9]+/g, '');
         valor = valor.val().replace(/[^.-]+/g, '');
         $(this).val(valor);
